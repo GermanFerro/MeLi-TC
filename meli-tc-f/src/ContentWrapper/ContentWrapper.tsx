419231a78a53b1b1c4aebc6deddcd74884531b9f
German Ferro <ferrogerman90@gmail.com>
2023-03-09 14:10:10 -0300

    improve loading state

--- meli-tc-f/src/ContentWrapper/ContentWrapper.tsx
+++ meli-tc-f/src/ContentWrapper/ContentWrapper.tsx
@@ -1,34 +1,23 @@
-import { Outlet, useNavigation, useRouteLoaderData } from "react-router-dom";
+import { Outlet, useRouteLoaderData } from "react-router-dom";
 import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
 import "./ContentWrapper.scss";
-
-const LoadingState = () => (
-  <div className="LoadingState">
-    <h1>Cargando</h1>
-  </div>
-);
 
 const SearchResults = () => {
   const listData = useRouteLoaderData("list") as any;
   const itemData = useRouteLoaderData("item") as any;
 
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
-
   return (
     <>
       <div className="row">
         <div className="col-12">
-          {!isLoading && (
-            <Breadcrumbs
-              categories={listData?.categories || itemData?.categories}
-            />
-          )}
+          <Breadcrumbs
+            categories={listData?.categories || itemData?.categories}
+          />
         </div>
       </div>
       <div className="row">
         <div className="col-12" style={{ marginTop: 0 }}>
-          {isLoading ? <LoadingState /> : <Outlet />}
+          <Outlet />
         </div>
       </div>
     </>
