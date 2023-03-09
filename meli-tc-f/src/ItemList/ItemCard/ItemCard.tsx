b0e5fe5d2086ac42ba01322ebdcb72ae9c5ca95a
German Ferro <ferrogerman90@gmail.com>
2023-03-08 22:49:12 -0300

    make list accessible

--- meli-tc-f/src/ItemList/ItemCard/ItemCard.tsx
+++ meli-tc-f/src/ItemList/ItemCard/ItemCard.tsx
@@ -1,3 +1,4 @@
+import { KeyboardEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import deliveryTruck from "../../assets/delivery-truck.png";
 import { ItemListNormal } from "../../types";
@@ -19,27 +20,27 @@ const ItemCard = ({
   const [integer] = formatPrice(price);
 
   return (
-    <div className="ItemCard">
+    <div
+      className="ItemCard"
+      tabIndex={0}
+      onKeyUp={(e: KeyboardEvent<HTMLInputElement>) =>
+        e.code === "Enter" && goToItemPage()
+      }
+      onClick={goToItemPage}
+    >
       <div className="col-3">
-        <img
-          className="thumbnail"
-          src={thumbnail}
-          alt="item thumbnail"
-          onClick={goToItemPage}
-        />
+        <img className="thumbnail" src={thumbnail} alt="item thumbnail" />
       </div>
       <div className="col-9">
         <div className="ItemDetails">
           <div className="groupedAttributes">
-            <div className="price" onClick={goToItemPage}>
+            <div className="price">
               <h3 className="font-regular">{integer}</h3>
               {freeShipping && (
                 <img className="deliveryTruck" src={deliveryTruck} alt="" />
               )}
             </div>
-            <h4 className="title font-regular" onClick={goToItemPage}>
-              {title}
-            </h4>
+            <h4 className="title font-regular">{title}</h4>
           </div>
           <h6 className="font-light text-xs">{address}</h6>
         </div>
